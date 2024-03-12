99b750256cba4f9509467eaa2eff23462e17e12c
Harshpreet Singh <94123019+harshpreet1123@users.noreply.github.com>
2024-03-12 18:55:02 +0530

    protected route

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -2,20 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req: any, res: Response, next: NextFunction) => {
+
   const token = req.header("Authorization")?.replace("Bearer ", "");
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  try {
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET || "secret");
-    req.userId = decoded._id;
+    const userId: string = decoded.userId;
+    req.userId=userId;
+    console.log(req.userId);
     next();
-  } catch (error) {
-    console.error(`Error: ${error}`);
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized or user not found" });
+    }
 };
 
 export default authMiddleware;
+
