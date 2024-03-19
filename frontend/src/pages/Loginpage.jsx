f556be08f44414d5d2c946cc7ad60fab10fe915a
Harsh <ha.rsh4334.r@gmail.com>
2024-03-19 23:18:02 +0530

    login ui update

--- frontend/src/pages/Loginpage.jsx
+++ frontend/src/pages/Loginpage.jsx
@@ -1,86 +1,74 @@
-import { Link } from "react-router-dom";
-
 const Loginpage = () => {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
-          BlockWork
-        </div>
-        <div className="mt-10 text-center text-xl leading-9 tracking-tight text-purple-600">
-          Sign in as a Client
+    <div className="h-screen flex">
+      <div className="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center">
+        <div>
+          <h1 className="text-white font-bold text-4xl font-sans">BlockWork</h1>
+          <p className="text-white mt-1">The Decentrlized Workplace.</p>
         </div>
       </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium leading-6 text-gray-900"
+      <div className="flex w-1/2 justify-center items-center bg-white">
+        <form className="bg-white">
+          <h1 className="text-gray-800 font-bold text-2xl mb-1">
+            Hello Again!
+          </h1>
+          <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              Email Address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full  shadow-sm focus:border-indigo-600 focus:ring-purple-600 focus:ring-opacity-50 py-2 placeholder-gray-400 focus:outline-none focus:ring-2  sm:text-sm border-solid border-2 p-2 border-purple-500 rounded-md "
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
               />
-            </div>
+            </svg>
+            <input
+              className="pl-2 outline-none border-none"
+              type="email"
+              name=""
+              id=""
+              placeholder="Email Address"
+              required
+            />
           </div>
-
-          <div>
-            <div className="flex items-center justify-between ">
-              <label
-                htmlFor="password"
-                className="block text-lg font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-purple-600 text-md hover:text-purple-800"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block border-solid border-2  border-purple-500 rounded-md  w-full shadow-sm focus:border-indigo-600 focus:ring-purple-600 focus:ring-opacity-50 py-2 placeholder-gray-400 focus:outline-none focus:ring-2  sm:text-sm"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className=" w-full h-full text-purple-600 text-xl font-bold border-solid border-2 p-2 border-purple-500 rounded-md py-1"
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
             >
-              Sign in
-            </button>
+              <path
+                fillRule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              className="pl-2 outline-none border-none"
+              type="password"
+              name=""
+              id=""
+              placeholder="Password"
+              required
+            />
           </div>
+          <button
+            type="submit"
+            className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+          >
+            Login
+          </button>
+          <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">
+            Forgot Password ?
+          </span>
         </form>
-        <p className="mt-8 text-center text-md text-gray-500">
-          don&apos;t have an account?{" "}
-          <Link to='/SignupPage'>
-            <a
-              href="#"
-              className="font-medium text-lg text-purple-600 hover:text-purple-800"
-            >
-              Sign Up
-            </a>
-            </Link>
-        </p>
       </div>
     </div>
   );
