e3f4b80ea3ed001917bbc1cedeb8f202e445d826
karan734079 <karanjangral60@gmail.com>
2024-03-23 10:21:20 +0530

    wsc

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -26,7 +26,7 @@ const Category = () => {
         <Link to={state.route} state={{ userType: "cl" }}>
           <button
             type="button"
-            className="text-white bg-gradient-to-tr from-blue-800 to-purple-700 hover:bg-gradient-to-tr hover:from-blue-700 hover:to-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-200  rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center font-mono font-bold"
+            className="text-white bg-gradient-to-tr from-blue-800 to-purple-700 hover:bg-gradient-to-tr hover:from-blue-700 hover:to-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-200  rounded-lg text-md px-5 py-2.5 inline-flex justify-center w-full text-center font-mono font-bold"
           >
             Client
           </button>
@@ -44,7 +44,7 @@ const Category = () => {
         <Link to={state.route} state={{ userType: "fr" }}>
           <button
             type="button"
-            className="text-white bg-gradient-to-tr from-blue-800 font-mono font-bold to-purple-700 hover:bg-gradient-to-tr hover:from-blue-700 hover:to-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-200  rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+            className="text-white bg-gradient-to-tr from-blue-800 font-mono font-bold to-purple-700 hover:bg-gradient-to-tr hover:from-blue-700 hover:to-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-200  rounded-lg text-md px-5 py-2.5 inline-flex justify-center w-full text-center"
           >
             Freelancer
           </button>
