836e83d777a030a6428d30f7fae3bca53a35275f
harshpreet1123 <ha.rsh4334.r@gmail.com>
2024-03-29 14:33:21 +0530

    ui fixes-> welcome animation, add profile client

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 // Set up mongoose connections
+const dbConString='mongodb://localhost:27017'
 let freelancerDB: mongoose.Connection;
 let clientDB: mongoose.Connection;
 let jobDB: mongoose.Connection;
 
 try {
   freelancerDB = mongoose.createConnection(
-    `mongodb://localhost:27017/freelancerDB`
+    `${dbConString}/freelancerDB`
   );
   console.log("Freelancer DB connceted");
 } catch (error) {
@@ -15,17 +16,19 @@ try {
 }
 
 try {
-  clientDB = mongoose.createConnection(`mongodb://localhost:27017/clientDB`);
+  clientDB = mongoose.createConnection(`${dbConString}/clientDB`);
   console.log('Client DB connected');
 } catch (error) {
   console.error("Error connecting to client database:", error);
 }
 
 try{
-  jobDB = mongoose.createConnection('mongodb://localhost:27017/jobsDB');
+  jobDB = mongoose.createConnection(`${dbConString}/jobsDB`);
   console.log('Jobs DB connected');
 }catch(error){
   console.error("Error Connecting to Jobs DB: ", error);
+}finally{
+  console.log(`DB running at ${dbConString}`);
 }
 
 // Export connections
