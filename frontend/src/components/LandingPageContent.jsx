cda8d549c115d93737b1528ca30e751f5642c403
Harsh <ha.rsh4334.r@gmail.com>
2024-04-03 08:00:51 +0530

    minor fixes

--- frontend/src/components/LandingPageContent.jsx
+++ frontend/src/components/LandingPageContent.jsx
@@ -3,9 +3,8 @@ import { gsap } from "gsap";
 import { Link } from "react-router-dom";
 import Spline from "@splinetool/react-spline";
 
-
-const Header=()=> {
-    const textRef = useRef(null);
+const LandingPageContent = () => {
+  const textRef = useRef(null);
   const buttonRef = useRef(null);
   const splineRef = useRef(null);
 
@@ -39,6 +38,7 @@ const Header=()=> {
   }, []);
   return (
     <div>
+      {/* Hero Section */}
       <header className="py-14">
         <div className="flex-half-row p-16">
           <div className="f-one" ref={textRef}>
@@ -52,7 +52,7 @@ const Header=()=> {
               recusandae laborum earum nobis voluptatum.
             </p>
             <br />
-            <Link to="/category" state={{route:'/register'}}>
+            <Link to="/category" state={{ route: "/register" }}>
               <a ref={buttonRef}>
                 <div className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 mx-2 -my-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                   <span className="relative px-3 py-2.5 me-0.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
@@ -65,14 +65,20 @@ const Header=()=> {
           </div>
           <div className="f-two">
             <Spline
-            ref={splineRef}
-            scene="https://prod.spline.design/60QZSgoHxT30HLIT/scene.splinecode"
-          />
+              ref={splineRef}
+              scene="https://prod.spline.design/60QZSgoHxT30HLIT/scene.splinecode"
+            />
           </div>
         </div>
       </header>
+      <br />
+      <div className="bg-black">
+        <p className="text-3xl justify-center items-center text-center text-white">
+          Dive into the World of decentrlization
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header;
+export default LandingPageContent;
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,13 @@
 import "../App.css";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+import LandingPageContent from "../components/LandingPageContent";
 import Navbar from "../components/Navbar";
 
 const LandingPage = () => {
   return (
     <div className="h-full w-full">
       <Navbar/>
-      <Header/>
+      <LandingPageContent/>
       <Footer/>
     </div>
   );
