5aa15b850dc67ffd85e72d7438b875bae15efed6
karan734079 <karanjangral60@gmail.com>
2024-04-22 11:35:17 +0530

    32f3

--- frontend/src/pages/client/Jobs.jsx
+++ frontend/src/pages/client/Jobs.jsx
@@ -7,7 +7,7 @@ const Jobs = () => {
       <p className=" font-semibold text-3xl text-gray-600 mx-10">Jobs</p>
       </div>
       <div className="flex justify-end">
-      <Link to="/CreateJob">
+      <Link to="/cl/home/CreateJob">
       <button className=" px-4 py-2 font-bold rounded bg-purple-600 text-gray-50">
         + CREATE JOB
       </button>
