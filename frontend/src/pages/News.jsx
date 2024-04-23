1c0e3572a633d0778c3f56f4fadbc1382b69e67e
karan734079 <karanjangral60@gmail.com>
2024-04-23 16:11:23 +0530

    km

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -21,7 +21,7 @@ const News = () => {
   return (
     <>
       <div className="p-8">
-        <h1 className="text-3xl flex justify-center font-bold text-white border-2 p-2 dark:bg-purple-600 rounded-xl">Top Crypto News</h1>
+        <h1 className="text-3xl flex justify-center font-bold text-slate-600 ">Top Crypto News</h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
         {newsData.map((article, index) => (
